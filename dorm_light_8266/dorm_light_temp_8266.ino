111b9790abb3889969667926eda3bc68121f82c8
Asimok <1962976634@qq.com>
2021-05-30 11:32:16 +0800

    向波毕设第三次修改

--- dorm_light_8266/dorm_light_temp_8266.ino
+++ dorm_light_8266/dorm_light_temp_8266.ino
@@ -1,41 +1,45 @@
-#include<Servo.h>
-#include "dht11.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <SoftwareSerial.h>
 #include "ArduinoJson-v6.15.2.h"
-#define dht11Pin D2   //定义温湿度针脚号为D2号引脚
+#define light0 D0   //定义开关1为D0号引脚
+#define light1 D1
+#define light2 D2
+#define light3 D3
 
-dht11 dht;    //实例化一个对象
-Servo left_servo, right_servo;
 long lastMsg = 0;
 char msg[500];
 int value = 0;
-
+//Wi-Fi配置
 const char* ssid = "女寝专用";
 const char* password = "208208nb";
+//MQTT服务器配置
 const char* mqtt_server = "39.96.68.13";
 const int mqttPort = 1883;
-const char* clientId = "4B208";
+const char* clientId = "xiangbo";
 const char* topic = "dorm";
-char* code = "closeLeft";
+
+char* code ;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-bool leftStatue = false;
-bool rightStatue = false;
+//4路开关实时状态
+bool light0Statue = false;
+bool light1Statue = false;
+bool light2Statue = false;
+bool light3Statue = false;
 
 void setup() {
-
 
   Serial.begin(9600);
   //连接wifi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    // Serial.println("Connecting to WiFi..");
+    Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
+
   //连接MQTT服务器
   client.setServer(mqtt_server, mqttPort);
   client.setCallback(callback);
@@ -49,7 +53,13 @@ void setup() {
       delay(2000);
     }
   }
-  pinMode(dht11Pin, OUTPUT);
+  //  继电器引脚设置为输出模式
+  pinMode(light0, OUTPUT);
+  pinMode(light1, OUTPUT);
+  pinMode(light2, OUTPUT);
+  pinMode(light3, OUTPUT);
+
+  // 设备上线提醒
   char buff[50];
   memset(buff, 0, sizeof(buff));
   const char *buff2 = " 上线";
@@ -57,39 +67,21 @@ void setup() {
   strcat(buff, buff2);
   //发送连接成功消息
   client.publish(topic, buff );
-  //订阅主题
+  //硬件设备终端订阅主题
   client.subscribe(topic);
+
   delay(100);
-  //  初始化开关状态
-  switchlight("closeLeft");
-  delay(1000);
-  switchlight("closeRight");
+  //初始化开关状态为关
+  switchlight("closeLight0");
+  delay(100);
+  switchlight("closeLight1");
+  delay(100);
+  switchlight("closeLight2");
+  delay(100);
+  switchlight("closeLight3");
+  delay(100);
 }
-void get_dht11()     //loop函数，重复循环执行
-{
-  int tol = dht.read(dht11Pin);    //将读取到的值赋给tol
-  int temp = (float)dht.temperature; //将温度值赋值给temp
-  int humi = (float)dht.humidity; //将湿度值赋给humi
-  //  Serial.print("Temperature:");     //在串口打印出Tempeature:
-  //  Serial.print(temp);       //在串口打印温度结果
-  //  Serial.println("℃");    //在串口打印出℃
-  //  Serial.print("Humidity:");     //在串口打印出Humidity:
-  //  Serial.print(humi);     //在串口打印出湿度结果
-  //  Serial.println("%");     //在串口打印出%
-  delay(10);      //延时1秒
 
-
-  StaticJsonDocument<200> temperature_data;
-
-  temperature_data["sensor"] = "DHT11";
-  temperature_data["temp"] = temp;
-  temperature_data["humi"] = humi;
-  serializeJson(temperature_data, msg);
-  //      Serial.println(msg);
-  client.publish(topic, msg );
-
-
-}
 /**
    断开重连
 */
@@ -97,7 +89,6 @@ void reconnect_mqtt() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.println("reConnecting to MQTT...");
-
     if (client.connect(clientId)) {
       Serial.println("connected");
     } else {
@@ -117,6 +108,7 @@ void reconnect_wifi()
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+
   //收到消息
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
@@ -126,96 +118,104 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
     a[i] = (char)payload[i];
   }
-
+  //解析指令
   docode(a);
 
 }
-void docode(char json[2000])
-{
-  //  "解析指令"
 
+  void docode(char json[2000])
+{
+  //解析指令
   StaticJsonDocument<200> doc;
   deserializeJson(doc, json);
   const char* code = doc["code"];
   Serial.println();
   Serial.println(code);
+
   String tempcode;
   tempcode = String(code);
-  if (tempcode == "get_dht11")
-    get_dht11();
-    else if (tempcode == "get_light_status")
-     send_light_data();
+  //  get_light_status 表示获取当前开关状态
+  if (tempcode == "get_light_status")
+    send_light_data();
   else
+    //执行开关灯指令
     switchlight(tempcode);
-
-
 }
 void switchlight(String tempcode)
 {
 
   delay(50);
-  if (tempcode == "openLeft")
+  //解析四路开关开灯指令
+  if (tempcode == "openLight0")
   {
-    left_servo.attach(D0);
-    left_servo.write(82);
-    delay(300);
-    left_servo.write(45);
-    delay(100);
-    left_servo.detach();
-    leftStatue = true;
-send_light_data();
-  }
-  else if (tempcode == "openRight")
-  {
-    right_servo.attach(D1);
-    right_servo.write(8);
-    delay(300);
-    right_servo.write(65);
-    delay(100);
-    right_servo.detach();
-    rightStatue = true;
+    digitalWrite(light0, HIGH);
+    light0Statue = true;
     send_light_data();
   }
-  else if (tempcode == "closeLeft")
+   if (tempcode == "openLight1")
   {
-    left_servo.attach(D0);
-    left_servo.write(2);
-    delay(300);
-    left_servo.write(45);
-    delay(100);
-    left_servo.detach();
-    leftStatue = false;
+    digitalWrite(light1, HIGH);
+    light1Statue = true;
+    send_light_data();
+  }
+   if (tempcode == "openLight2")
+  {
+    digitalWrite(light2, HIGH);
+    light2Statue = true;
     send_light_data();
 
   }
-  else if (tempcode == "closeRight")
+   if (tempcode == "openLight3")
   {
-    right_servo.attach(D1);
-    right_servo.write(130);
-    delay(300);
-    right_servo.write(65);
-    delay(100);
-    right_servo.detach();
-    rightStatue = false;
+    digitalWrite(light3, HIGH);
+    light3Statue = true;
     send_light_data();
   }
 
-  
+//解析四路开关关灯指令
+  if (tempcode == "closeLight0")
+  {
+    digitalWrite(light0, HIGH);
+    light0Statue = false;
+    send_light_data();
+  }
+   if (tempcode == "closeLight1")
+  {
+    digitalWrite(light1, HIGH);
+    light1Statue = false;
+    send_light_data();
+  }
+   if (tempcode == "closeLight2")
+  {
+    digitalWrite(light2, HIGH);
+    light2Statue = false;
+    send_light_data();
+  }
+   if (tempcode == "closeLight3")
+  {
+    digitalWrite(light3, HIGH);
+    light3Statue = false;
+    send_light_data();
+  }
 
 
 }
 
-void send_light_data(){
+void send_light_data() {
   delay(10);
 
   StaticJsonDocument<200> light_data;
-  light_data["sensor"] = "servo";
-  light_data["left"] = leftStatue;
-  light_data["right"] = rightStatue;
+  light_data["sensor"] = "light";
+
+  light_data["light0"] = light0Statue;
+  light_data["light1"] = light1Statue;
+  light_data["light2"] = light2Statue;
+  light_data["light3"] = light3Statue;
   serializeJson(light_data, msg);
-  //      Serial.println(msg);
+  Serial.println(msg);
   client.publish(topic, msg );
-  delay(100);}
+  delay(100);
+}
 void loop() {
 
   //重连机制
